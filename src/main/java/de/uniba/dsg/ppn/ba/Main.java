638ea863a45a6ee20cfb2bffa3ce6316ab8bba9e
Philipp Neugebauer <ppn@nexgo.de>
2014-12-17 21:17:48 +0100

    license text printing improved

--- src/main/java/de/uniba/dsg/ppn/ba/Main.java
+++ src/main/java/de/uniba/dsg/ppn/ba/Main.java
@@ -40,7 +40,7 @@ public class Main {
 
     private final static Logger LOGGER;
     private final static Level DEBUGLEVEL;
-    private final static String LICENSE = "SchematronBPMNValidator  Copyright (C) 2014  Philipp Neugebauer\n This program comes with ABSOLUTELY NO WARRANTY; This is free software, and you are welcome to redistribute it under certain conditions; See LGPLv3.";
+    private final static String LICENSE = "SchematronBPMNValidator  Copyright (C) 2014  Philipp Neugebauer\nThis program comes with ABSOLUTELY NO WARRANTY; This is free software, and you are welcome to redistribute it under certain conditions; See LGPLv3.";
 
     static {
         DEBUGLEVEL = Level.DEBUG;
@@ -48,7 +48,7 @@ public class Main {
     }
 
     public static void main(String... args) {
-        System.out.println(LICENSE);
+        LOGGER.info(LICENSE);
         SchematronBPMNValidator validator = new SchematronBPMNValidator();
         ArrayList<String> argsAsList = new ArrayList<>(Arrays.asList(args));
         XmlWriter xmlWriter = new XmlWriter();
